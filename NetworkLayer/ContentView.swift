b0f4e1fd5a92ca25995e465c1b0941933f6f3af2
Viswanath Reddy Ponthota (Contractor) <viswanathreddy.ponthota@cognizant.com>
2024-08-01 09:48:00 +0530

    Initial Commit

---
+++ NetworkLayer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NetworkLayer
+//
+//  Created by Ponthota, Viswanath Reddy (Cognizant) on 01/08/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NetworkLayer/NetworkLayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkLayerApp.swift
+//  NetworkLayer
+//
+//  Created by Ponthota, Viswanath Reddy (Cognizant) on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkLayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
